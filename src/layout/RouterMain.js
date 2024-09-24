c9ece7e506b6cad4360122814549f365e840662f
HoseinGholami <139072268+hoseingh7@users.noreply.github.com>
2024-09-24 15:05:05 -0700

    edit router file

--- src/layout/RouterMain.js
+++ src/layout/RouterMain.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../Pages/Home/Home";
 import { Login } from "../Pages/Login/Login";
+import { Dashboard } from "../Dashboard/Dashboard";
 const RouterMain = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/" element={<Home />} />
         <Route element={<Login/>} path="/login"/>
       </Routes>
